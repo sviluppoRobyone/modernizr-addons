6f3b309e642ac878676e25bd64715adcce719d56
Gianluca Cagnin <gianluca.cagnin@robyone.net>
2015-07-15 16:33:50 +0200

    first commit

---
+++ modernizr-browser.js
@@ -0,0 +1,10 @@
+(function () {
+    var ua = navigator.userAgent.toLowerCase();
+    Modernizr.addTest("ie", function () { return ua.indexOf("msie") !== -1; });
+    [6, 7, 8, 9, 10, 11].forEach(function (versionNumber) {
+        Modernizr.addTest("ie" + versionNumber, function () { return navigator.userAgent.indexOf("msie " + versionNumber) !== -1; });
+    });
+    Modernizr.addTest("chrome", function () { return ua.indexOf("chrome") !== -1; });
+    Modernizr.addTest("firefox", function () { return ua.indexOf("firefox") !== -1; });
+})();
+//# sourceMappingURL=modernizr-browser.js.map
---
+++ modernizr-browser.ts
@@ -0,0 +1,22 @@
+﻿interface IModernizr{
+    addTest(name:string,testFn:{():boolean}):void
+}
+declare var Modernizr:IModernizr;
+
+(() => {
+    var ua = navigator.userAgent.toLowerCase();
+
+    Modernizr.addTest("ie", () => ua.indexOf("msie") !== -1);
+
+    [6, 7, 8, 9, 10, 11].forEach(versionNumber => {
+        Modernizr.addTest("ie" + versionNumber, () => navigator.userAgent.indexOf("msie " + versionNumber) !== -1);
+    });
+
+    Modernizr.addTest("chrome", () => ua.indexOf("chrome") !== -1);
+    Modernizr.addTest("firefox", () => ua.indexOf("firefox") !== -1);
+
+
+
+
+})();
+
