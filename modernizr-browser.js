4209fb1f0391ee1453a01c9ba52dd32fa818b335
gianluca.cagnin <gianluca.cagnin@robyone.net>
2017-10-09 12:36:20 +0200

    aggiornamenti vari

--- modernizr-browser.js
+++ modernizr-browser.js
@@ -1,10 +1,13 @@
 (function () {
-    var ua = navigator.userAgent.toLowerCase();
-    Modernizr.addTest("ie", function () { return ua.indexOf("msie") !== -1; });
-    [6, 7, 8, 9, 10, 11].forEach(function (versionNumber) {
-        Modernizr.addTest("ie" + versionNumber, function () { return navigator.userAgent.indexOf("msie " + versionNumber) !== -1; });
-    });
-    Modernizr.addTest("chrome", function () { return ua.indexOf("chrome") !== -1; });
-    Modernizr.addTest("firefox", function () { return ua.indexOf("firefox") !== -1; });
+    if (!!window["Modernizr"]) {
+        var ua = navigator.userAgent.toLowerCase();
+        Modernizr.addTest("ie", function () { return ua.indexOf("msie") !== -1; });
+        [6, 7, 8, 9, 10, 11, 12].forEach(function (versionNumber) {
+            Modernizr.addTest("ie" + versionNumber, function () { return navigator.userAgent.indexOf("msie " + versionNumber) !== -1; });
+        });
+        Modernizr.addTest("edge", function () { return ua.indexOf("edge") !== -1; });
+        Modernizr.addTest("chrome", function () { return ua.indexOf("chrome") !== -1; });
+        Modernizr.addTest("firefox", function () { return ua.indexOf("firefox") !== -1; });
+    }
 })();
 //# sourceMappingURL=modernizr-browser.js.map
--- modernizr-browser.ts
+++ modernizr-browser.ts
@@ -4,18 +4,19 @@
 declare var Modernizr:IModernizr;
 
 (() => {
-    var ua = navigator.userAgent.toLowerCase();
+    if (!!window["Modernizr"]) {
+        var ua = navigator.userAgent.toLowerCase();
 
-    Modernizr.addTest("ie", () => ua.indexOf("msie") !== -1);
+        Modernizr.addTest("ie", () => ua.indexOf("msie") !== -1);
 
-    [6, 7, 8, 9, 10, 11].forEach(versionNumber => {
-        Modernizr.addTest("ie" + versionNumber, () => navigator.userAgent.indexOf("msie " + versionNumber) !== -1);
-    });
+        [6, 7, 8, 9, 10, 11, 12].forEach(versionNumber => {
+            Modernizr.addTest(`ie${versionNumber}`, () => navigator.userAgent.indexOf(`msie ${versionNumber}`) !== -1);
+        });
+        Modernizr.addTest("edge", () => ua.indexOf("edge") !== -1);
+        Modernizr.addTest("chrome", () => ua.indexOf("chrome") !== -1);
+        Modernizr.addTest("firefox", () => ua.indexOf("firefox") !== -1);
 
-    Modernizr.addTest("chrome", () => ua.indexOf("chrome") !== -1);
-    Modernizr.addTest("firefox", () => ua.indexOf("firefox") !== -1);
-
-
+    }
 
 
 })();
